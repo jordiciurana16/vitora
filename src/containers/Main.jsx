64dd7ce0be9dcdef6562ffc1eca8b6d3f5223501
jordiciurana16 <jciuranas@gmail.com>
2024-03-01 18:20:34 +0100

    01/03/2024

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,13 +1,46 @@
-import React from 'react';
-import { Container, Row, Col} from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { fetchCountriesData } from '../services/api/countriesData'; // Importa la funció fetchCountriesData
 
 function Main() {
+  const [countriesDataFormatted, setCountriesDataFormatted] = useState([]); // Estat local per emmagatzemar les dades dels països
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await fetchCountriesData('SP.POP.TOTL'); // Crida la funció fetchCountriesData
+        setCountriesDataFormatted(data); // Actualitza l'estat local amb les dades obtingudes
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchData(); // Crida la funció per obtenir les dades quan el component es munti
+  }, []);
+
   return (
     <main>
       <Container fluid>
         <Row>
           <Col className='px-0'>
-            <p>Main component</p>
+            <table>
+              <thead>
+                <tr>
+                  <th>Country ID</th>
+                  <th>Country Name</th>
+                  <th>Life Expectancy</th>
+                </tr>
+              </thead>
+              <tbody>
+                {countriesDataFormatted.map((country, index) => (
+                  <tr key={index}>
+                    <td>{country.id}</td>
+                    <td>{country.name}</td>
+                    <td>{country.lifeExpectancy}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </Col>
         </Row>
       </Container>
