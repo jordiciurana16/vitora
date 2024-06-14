abbd5cbffc8c56a14939ae46a625211e9ad9a9ee
jordiciurana16 <jciuranas@gmail.com>
2024-06-14 15:28:52 +0200

    hashroute

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { GlobalProvider } from './hooks/GlobalContext';
 import LandingPage from './pages/LandingPage';
 import DashboardPage from './pages/DashboardPage';
@@ -9,7 +9,7 @@ import ResourcesPage from './pages/ResourcesPage';
 
 function App() {
   return (
-    <Router basename="/vitora">
+    <Router>
       <GlobalProvider>
         <Routes>
           <Route path="/" element={<LandingPage />} />
